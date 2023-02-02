c095dba478f9e8823eb4aa359ef8b614e53b0b3a
Kazetus <105437345+Kazetus@users.noreply.github.com>
2023-02-02 09:29:27 +0100

    S

--- 3w/projet_final/test.php
+++
@@ -1,4 +0,0 @@
-<?php
-var_dump(mime_content_type('img/Matthias/avatar.jpeg'));
-var_dump(mime_content_type('img/Matthias/text.jpg'));
-var_dump(mime_content_type('img/Piranha_Plant_3D_World.webp'));
