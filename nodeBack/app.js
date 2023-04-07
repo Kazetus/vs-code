f4520fb93e68c10ef0b6bdb590d51719e1629403
Kazetus <105437345+Kazetus@users.noreply.github.com>
2023-04-07 16:32:52 +0200

    d

--- nodeBack/app.js
+++ nodeBack/app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-
+const humain = {
+    name:"Matthias",
+    age:32
+}
+const tache = [
+    {
+        titre: "Apprendre Node",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptate veniam, corporis ex vero consequatur illum doloremque laborum architecto, quia nemo exercitationem tenetur possimus dicta eaque quibusdam mollitia dolore earum."
+    },
+    {
+        titre: "Apprendre Express",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptate veniam, corporis ex vero consequatur illum doloremque laborum architecto, quia nemo exercitationem tenetur possimus dicta eaque quibusdam mollitia dolore earum."
+    }
+]
 app.use(express.static('public'));
 app.set('views', './IHM');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.status(200).render('index');
+    res.status(200).render('index', {humain, tache});
 })
 app.get('/a-propos', (req, res) => {
     res.status(200).render('apropos');
