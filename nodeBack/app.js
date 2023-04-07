9a64acec268b5fe0f0be8bfd057f65a37137b7d1
Kazetus <105437345+Kazetus@users.noreply.github.com>
2023-04-07 17:12:53 +0200

    e

--- nodeBack/app.js
+++ nodeBack/app.js
@@ -1,27 +1,36 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const humain = {
-    name:"Matthias",
-    age:32
-}
-const tache = [
-    {
-        titre: "Apprendre Node",
-        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptate veniam, corporis ex vero consequatur illum doloremque laborum architecto, quia nemo exercitationem tenetur possimus dicta eaque quibusdam mollitia dolore earum."
-    },
-    {
-        titre: "Apprendre Express",
-        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptate veniam, corporis ex vero consequatur illum doloremque laborum architecto, quia nemo exercitationem tenetur possimus dicta eaque quibusdam mollitia dolore earum."
-    }
-]
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
+const optionBDD = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'todolist'
+};
+
+app.use(myConnection(mysql, optionBDD, 'pool'));
 app.use(express.static('public'));
 app.set('views', './IHM');
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.status(200).render('index', {humain, tache});
-})
+app.get('/', (req, res)=>{
+    req.getConnection((error, connection)=>{
+      if (error) {
+        console.error(error);
+      } else {
+        connection.query('SELECT * FROM liste', [], (error, data)=>{
+          if (error) {
+            console.error(error);
+          } else {
+            res.status(200).render('index', {data})
+          }
+        })
+      }
+    })
+});
 app.get('/a-propos', (req, res) => {
     res.status(200).render('apropos');
 })
