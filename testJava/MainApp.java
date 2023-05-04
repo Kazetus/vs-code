210fda0accfb42e07e57db8b3bffaad9ad538d0e
Kazetus <105437345+Kazetus@users.noreply.github.com>
2023-05-04 09:19:00 +0200

    java

---
+++ testJava/MainApp.java
@@ -0,0 +1,34 @@
+public class MainApp {
+    public static void main(String[] args) {
+        final float PI = 3.14f;
+        System.out.println(PI);
+        final byte min = 127;
+        System.out.println(min);
+        min = (int) min;
+        final double Pie = 3.1415;
+        System.out.println(Pie);
+    }
+}
+// Pour un commentaire sur une seule ligne
+
+/*
+ * Pour les commentaires sur plusieurs
+ *  lignes.
+ * \n : retour à lligne
+ * \t : tabulation
+ * \r : retour chariot
+ * \b : retour arrière
+ * \f : nouvelle page (file)
+ * base numerique : décimale (10 -> 0 à 9)
+ *                  binaire (2 -> 0 à 1)
+ *                  octale (8 -> 0 à 7)
+ *                  hexadecimale  (16 -> 0 à F)
+ * nombre entier : 12
+ *                 12_333
+ *                 12333
+ * binaire -> 0b1100011
+ *              1_100_011
+ * hexadecimale -> 0xFB233
+ * 
+ * pas de caracteres speciaux, pas d'espaces, commence pas une lettre ou un _
+ */
